f0ca1b7a5338b86520f0d1b74310b2646f034d9a
taircode <t.akhmejanov@gmail.com>
2021-11-21 23:39:05 -0600

    rearranged file structure

---
+++ createDataSplits.py
@@ -0,0 +1,8 @@
+
+
+#first merge all of the CSVs
+
+#from stackoverflow
+train_size = int(0.8 * len(full_dataset))
+test_size = len(full_dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
