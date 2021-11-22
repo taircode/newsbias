f3d02a5134af5dcb7bce7eee39885310655b641e
taircode <t.akhmejanov@gmail.com>
2021-11-21 22:46:11 -0600

    Azure Upload

---
+++ azureUpload.py
@@ -0,0 +1,11 @@
+from azureml.core import Workspace
+"Upload training data to azure blob storage"
+
+print("Connecting to Azure ML Workspace from config.json...")
+ws = Workspace.from_config()
+
+print("Connecting to workspace default datastore...")
+ds = ws.get_default_datastore()
+
+print("Uploading train articles")
+ds.upload(src_dir="./articles",target_path="./news/articles",overwrite=True)
