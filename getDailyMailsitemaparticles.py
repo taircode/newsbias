9d125925b3eb911efec02a8fe2f792568881e2ca
taircode <t.akhmejanov@gmail.com>
2021-11-24 00:50:43 -0600

    get Daily Mail working

--- getDailyMailsitemaparticles.py
+++ getDailyMailsitemaparticles.py
@@ -52,31 +52,44 @@ for current_link in links:
         #print(div)
         #print("HERE ARE THE ATTRIBUTES OF THE FIRST P")
         #print(div.find("p").attrs)
-        all_ps=div.find_all("p", {"class": "mol-para-with-font"})
+        #this is why find_all is probably better, so you get an empty list rather than a single None-type
+        if div is not None:
+            all_ps=div.find_all("p", {"class": "mol-para-with-font"})
 
-        """Here's the one line code I had before. The issue is that you might get a p tag that doesn't have a class attribute at all"""
-        """There's probably a way to deal with this in the single line"""
-        #body_paragraphs=div.find_all(lambda tag: tag.name == 'p' and 'imageCaption' not in tag['class'])
+            """Here's the one line code I had before. The issue is that you might get a p tag that doesn't have a class attribute at all"""
+            """There's probably a way to deal with this in the single line"""
+            #body_paragraphs=div.find_all(lambda tag: tag.name == 'p' and 'imageCaption' not in tag['class'])
 
-        #body_paragraphs=[]
-        #for item in all_ps:
-        #    if 'class' in item.attrs:
-        #        if 'imageCaption' not in item['class']:
-        #            body_paragraphs.append(item)
-        #    else:
-        #        print(f"Here are the attributes of a p without class {item.attrs}")
+            #body_paragraphs=[]
+            #for item in all_ps:
+            #    if 'class' in item.attrs:
+            #        if 'imageCaption' not in item['class']:
+            #            body_paragraphs.append(item)
+            #    else:
+            #        print(f"Here are the attributes of a p without class {item.attrs}")
 
-        bodytext=[e.get_text() for e in all_ps]
+            bodytext=[e.get_text() for e in all_ps]
 
-        if len(bodytext)==0:
-            print("EMPTY ARTICLE")
-        else:
-            #how do we want to join these paragraph? 
-            article = '\n'.join(bodytext)
-            articles.append(article)
-            count=count+1
-            if count%100==0:
-                print(count)
-                print(f"Article title: {title}")
-                print(current_link)
-                print(article)
+            if len(bodytext)==0:
+                print("EMPTY ARTICLE")
+            else:
+                #how do we want to join these paragraph? 
+                article = '\n'.join(bodytext)
+                articles.append(article)
+                count=count+1
+                if count%100==0:
+                    print(count)
+                    print(f"Article title: {title}")
+                    print(current_link)
+                    print(article)
+
+labels=[3]*count
+print(labels)
+
+#this should have a list of lists that is [article, label] pairs
+list_of_datapairs = list(zip(articles, labels))
+
+df = pd.DataFrame(list_of_datapairs, columns=["article","label"])
+datafile = df.to_csv("scrapedData/DailyMaildata.csv")
+
+#implement append version down here if you want - see geteconomistarticles
