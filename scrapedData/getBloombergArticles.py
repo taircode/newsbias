54e19bfcbbb07bf67405a35c9bc570dc4ed53a01
taircode <t.akhmejanov@gmail.com>
2021-11-27 21:43:38 -0600

    get MSNBC articles works

--- scrapedData/getBloombergArticles.py
+++ scrapedData/getBloombergArticles.py
@@ -11,7 +11,7 @@ loc_tags=main_soup.find_all('loc')
 links=[]
 link_count=0
 for loc in loc_tags:
-    if link_count>1000:
+    if link_count>10:
         break
     sitemap=loc.get_text()
     print(sitemap)
@@ -25,5 +25,46 @@ for loc in loc_tags:
             links.append(cur_link)
             link_count=link_count+1
             print(link_count)
-            if link_count>1000:
+            if link_count>10:
                 break
+
+
+#<div class="body-copy-v2"
+
+
+print(f"Found {len(links)} many links.")
+titles=[]
+articles=[]
+count=0
+
+for link in links:
+    print(link)
+    r=requests.get(link,timeout=10)
+    html_source=r.text
+    print(html_source)
+    #various options for parsers: "html.parser"
+    #class="body-copy fence-body"
+    soup=BeautifulSoup(html_source,"lxml")
+    p_tags=soup.find_all("div",{'class': "body-copy fence-body"})
+    for p in p_tags:
+        print(p.prettify())
+    exit()
+    if div is not None:
+        article_body=div.get_text()
+        print(article_body)
+        #print(article_body)
+        articles.append(article_body)
+        count=count+1
+        if count%100==0:
+            print(count)
+
+print(f"count={count}")
+
+labels=[0]*count
+print(labels)
+
+#this should have a list of lists that is [article, label] pairs
+list_of_datapairs = list(zip(articles, labels))
+
+df = pd.DataFrame(list_of_datapairs, columns=["article","label"])
+datafile = df.to_csv("raw_articles/Bloomber.csv")
--- scrapedData/getMSNBCarticles.py
+++ scrapedData/getMSNBCarticles.py
@@ -7,39 +7,48 @@ import pandas as pd
 #Use this for all of the years and months
 
 #If you want more data, use the other months
-sitemaps=["https://www.msnbc.com/archive/articles/2021/september","https://www.msnbc.com/archive/articles/2021/october"]
-links=[]
-
-for map in sitemaps:
-    sitemap_request=requests.get(map)
-    sitemap_soup=BeautifulSoup(sitemap_request.text,'lxml')
-    main=sitemap_soup.find('main')
-    a_tags=main.find_all('a')
-    for a_tag in a_tags:
-        links.append(a_tag.get('href'))
-
-print(f"Found {len(links)} many links.")
+sitemaps=["https://www.msnbc.com/archive/articles/2021/october",
+"https://www.msnbc.com/archive/articles/2021/september",
+"https://www.msnbc.com/archive/articles/2021/august",
+"https://www.msnbc.com/archive/articles/2021/july",
+"https://www.msnbc.com/archive/articles/2021/june",
+"https://www.msnbc.com/archive/articles/2021/may",
+"https://www.msnbc.com/archive/articles/2021/april",
+"https://www.msnbc.com/archive/articles/2021/march",
+"https://www.msnbc.com/archive/articles/2021/february",
+"https://www.msnbc.com/archive/articles/2021/january",
+]
 
 titles=[]
 articles=[]
 count=0
 
-for link in links:
-    if 'transcript' not in link:
-        print(link)
-        r=requests.get(link,timeout=10)
-        html_source=r.text
+for map in sitemaps:
+    if len(articles)>1000:
+        break
+    sitemap_request=requests.get(map)
+    sitemap_soup=BeautifulSoup(sitemap_request.text,'lxml')
+    main=sitemap_soup.find('main')
+    a_tags=main.find_all('a')
+    for a_tag in a_tags:
+        if len(articles)>1000:
+            break
+        link=a_tag.get('href')
+        if 'transcript' not in link:
+            print(link)
+            r=requests.get(link,timeout=10)
+            html_source=r.text
 
-        #various options for parsers: "html.parser"
-        soup=BeautifulSoup(html_source,"lxml")
+            #various options for parsers: "html.parser"
+            soup=BeautifulSoup(html_source,"lxml")
 
-        div=soup.find("div",{'class': 'article-body__content article-body-font--loading'})
-        article_body=div.get_text()
-        #print(article_body)
-        articles.append(article_body)
-        count=count+1
-        if count%100==0:
-            print(count)
+            div=soup.find("div",{'class': 'article-body__content article-body-font--loading'})
+            article_body=div.get_text()
+            #print(article_body)
+            articles.append(article_body)
+            count=count+1
+            if count%100==0:
+                print(count)
 
 print(f"count={count}")
 
