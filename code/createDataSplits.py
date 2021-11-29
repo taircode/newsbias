3d7a9902ff7bd74dfca9f46f6630f913d9c6d72e
taircode <t.akhmejanov@gmail.com>
2021-11-28 21:39:23 -0600

    using a dict in createDataSplits

--- code/createDataSplits.py
+++ code/createDataSplits.py
@@ -22,62 +22,61 @@ msnbc=pd.read_csv("../scrapedData/raw_articles/MSNBCData.csv", usecols=['article
 dailymail=pd.read_csv("../scrapedData/raw_articles/DailyMailData.csv", usecols=['article','label'])
 fox=pd.read_csv("../scrapedData/raw_articles/FoxNewsData.csv", usecols=['article','label'])
 
-all_frames=[reuters,economist,newyorker,nationalreview,cnn,msnbc,dailymail,fox]
+frames_dict={
+    "reuters": reuters,
+    "economist": economist,
+    "newyorker": newyorker,
+    "nationalreview": nationalreview,
+    "cnn": cnn,
+    "msnbc": msnbc,
+    "dailymail": dailymail,
+    "fox": fox,
+}
+
 #reuters=reuters.rename(columns={'label': 'labels'})
 #economist=economist.rename(columns={'label': 'labels'})
 #cnn=cnn.rename(columns={'label': 'labels'})
 #dailymail=dailymail.rename(columns={'label': 'labels'})
 
-print(dailymail['article'])
-for frame in all_frames:
-    nulls=np.where(pd.isnull(frame['article']))
-    frame=frame.drop(nulls[0])
-print(dailymail['article'])
-exit()
 #making sure that there are no empty strings, i.e. NaNs, DailyMail was letting some slip through
-nulls=np.where(pd.isnull(dailymail['article']))
-dailymail=dailymail.drop(nulls[0])
-#print(len(nulls[0]))
-
-nulls=np.where(pd.isnull(economist['article']))
-economis=economist.drop(nulls[0])
-#print(len(nulls[0]))
-
-nulls=np.where(pd.isnull(cnn['article']))
-cnn=cnn.drop(nulls[0])
-#print(len(nulls[0]))
-
-nulls=np.where(pd.isnull(reuters['article']))
-reuters=reuters.drop(nulls[0])
-#print(len(nulls[0]))
+for key in frames_dict:
+    nulls=np.where(pd.isnull(frames_dict[key]['article']))
+    frames_dict[key]=frames_dict[key].drop(nulls[0])
 
 #get rid of the city, country '(Reuters) - ' beginnings
-for idx in range(len(reuters)):
-    current = reuters.loc[idx,'article']
+for idx in range(len(frames_dict['reuters'])):
+    current = frames_dict['reuters'].loc[idx,'article']
     index=current.find('(Reuters) - ')
     if index != -1:
-        reuters.loc[idx,'article']=current[index+len('(Reuters) - '):]
+        frames_dict['reuters'].loc[idx,'article']=current[index+len('(Reuters) - '):]
 
 #get rid of the city, country '(CNN)' beginnings
-for idx in range(len(cnn)):
-    current = cnn.loc[idx,'article']
+for idx in range(len(frames_dict['cnn'])):
+    current = frames_dict['cnn'].loc[idx,'article']
     index=current.find('(CNN)')
     if index != -1:
-        cnn.loc[idx,'article']=current[index+len('(CNN)'):]
+        frames_dict['cnn'].loc[idx,'article']=current[index+len('(CNN)'):]
     else:
         index=current.find('(CNN Business)')
         if index != -1:
-            cnn.loc[idx,'article']=current[index+len('(CNN Business)'):]
+            frames_dict['cnn'].loc[idx,'article']=current[index+len('(CNN Business)'):]
 
 #The economist and dailymail don't have these beginnings
 
 #this should have already been done when collecting the articles - maybe the labelling should be done here
-reuters = reuters.assign(label=0)
-economist = economist.assign(label=1)
-cnn = cnn.assign(label=2)
-dailymail = dailymail.assign(label=3)
+frames_dict['reuters'] = frames_dict['reuters'].assign(label=0)
 
-full_dataset=pd.concat([reuters,economist,cnn,dailymail], ignore_index=True)
+frames_dict['economist'] = frames_dict['reuters'].assign(label=1)
+frames_dict['newyorker'] = frames_dict['newyorker'].assign(label=1)
+frames_dict['nationalreview'] = frames_dict['nationalreview'].assign(label=1)
+
+frames_dict['cnn'] = frames_dict['cnn'].assign(label=2)
+frames_dict['msnbc'] = frames_dict['msnbc'].assign(label=2)
+
+frames_dict['dailymail'] = frames_dict['dailymail'].assign(label=3)
+frames_dict['fox'] = frames_dict['fox'].assign(label=3)
+
+full_dataset=pd.concat(list(frames_dict.values()), ignore_index=True)
 
 print(full_dataset)
 
@@ -90,6 +89,8 @@ train, val = np.split(permuted_dataset,[int(.8*len(permuted_dataset))])
 print(f"Full dataset size is {len(permuted_dataset)}")
 print(f"Train dataset size is {len(train)}")
 print(f"Eval dataset size is {len(val)}")
+
+exit()
 
 train.to_csv("../articles/train.csv")
 val.to_csv("../articles/eval.csv")
