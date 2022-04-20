a2fd446fe30d8596648e1cf40f16e33cbf569993
taircode <t.akhmejanov@gmail.com>
2022-04-20 14:10:29 -0500

    ran model

--- code/run_model.py
+++ code/run_model.py
@@ -9,22 +9,24 @@ print("Connecting to workspace datastore...")
 ws = Workspace.from_config()
 ds = ws.get_default_datastore()
 
-print(f"Downloading model to news/model from news/model")
-ds.download(
-    target_path="..",
-    prefix="news/model/pytorch_model.bin",
-    overwrite=False,
-    show_progress=True
-)
+print(f"Downloading config.json to local news/model from news/model")
 ds.download(
     target_path="..",
     prefix="news/model/config.json",
     overwrite=False,
     show_progress=True
 )
+print(f"Downloading training_args.bin to local news/model from news/model")
 ds.download(
     target_path="..",
     prefix="news/model/training_args.bin",
+    overwrite=False,
+    show_progress=True
+)
+print(f"Downloading pytorch_model.bin to local news/model from news/model")
+ds.download(
+    target_path="..",
+    prefix="news/model/pytorch_model.bin",
     overwrite=False,
     show_progress=True
 )
@@ -35,7 +37,7 @@ print("Loading tokenizer...")
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
 #is the entire article a single line? If not, this should probably be readlines()
-with open("../articles_test/ap_article2.txt","r") as file:
+with open("../articles_test/ap_article.txt","r") as file:
     all_lines=file.readlines()
     text_input=' '.join(all_lines)
 
