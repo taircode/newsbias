f8cb5210bae3013470a2be4ffa32b02d12acc141
taircode <t.akhmejanov@gmail.com>
2022-04-22 12:56:31 -0500

    documentation

--- code/run_model.py
+++ code/run_model.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 #from scipy.special import softmax
 import torch
 from azureml.core import Workspace
+import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--type","-t",choices=["huggingface","pytorch"],default='huggingface',help="select training framework") #huggingface or pytorch
@@ -18,21 +20,21 @@ print("Connecting to workspace datastore...")
 ws = Workspace.from_config()
 ds = ws.get_default_datastore()
 
-print(f"Downloading config.json to local news/model from news/model")
+print(f"Downloading config.json to local {location} from datastroe {location}")
 ds.download(
     target_path="..",
     prefix=location+"/config.json",
     overwrite=False,
     show_progress=True
 )
-print(f"Downloading training_args.bin to local news/model from news/model")
+print(f"Downloading training_args.bin to local {location} from datastroe {location}")
 ds.download(
     target_path="..",
     prefix=location+"/training_args.bin",
     overwrite=False,
     show_progress=True
 )
-print(f"Downloading pytorch_model.bin to local news/model from news/model")
+print(f"Downloading pytorch_model.bin to local {location} from datastroe {location}")
 ds.download(
     target_path="..",
     prefix=location+"/pytorch_model.bin",
@@ -45,38 +47,40 @@ model=AutoModelForSequenceClassification.from_pretrained("../"+location)
 print("Loading tokenizer...")
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
-#is the entire article a single line? If not, this should probably be readlines()
-with open("../articles_test/ap_article.txt","r") as file:
-    all_lines=file.readlines()
-    text_input=' '.join(all_lines)
-
-print("Here is the input text:\n")
-print(text_input)
-
-print("Tokenizing input...\n")
-
-tokenized_input=tokenizer(text_input, padding=True, truncation=True, return_tensors="pt")
-print(tokenized_input)
-
-print("Running model to get tokenized output...\n")
-output=model(**tokenized_input)
-#print(tokenized_output)
-#print("\n")
-#print(tokenized_output[0])
-
-print(f"output is {output}")
-print(f"SoftMax is {F.softmax(output[0],dim=-1)}")
-#print(f"softmax is {softmax(output)}")
-
-
 eval=pd.read_csv("../articles/eval.csv", usecols=['article','label'])
 for id in range(len(eval)):
     text=eval.loc[id,'article']
     label=eval.loc[id,'label']
 
     tokenized_text=tokenizer(text, padding=True, truncation=True,return_tensors="pt")
-    print(tokenized_text)
-    output=model(**tokenized_text)
-    print(f"output is {output}")
-    print(f"SoftMax is {F.softmax(output[0],dim=1)}")
+    
+    with torch.no_grad():
+        output=model(**tokenized_text)
+
+    logits = output.logits
+    softmax=torch.nn.Softmax(logits)
+    prediction = np.argmax(logits, axis=-1)
+
+    #print(f"output is {max(output[0])}")
+    #print(f"SoftMax is {softmax}")
+    print(f"Prediction is {prediction}")
     print(f"Actual label was {label}")
+
+#if you want to just look at one article
+#with open("../articles_test/ap_article.txt","r") as file:
+#    all_lines=file.readlines()
+#    text_input=' '.join(all_lines)
+
+#print("Here is the input text:\n")
+#print(text_input)
+
+#print("Tokenizing input...\n")
+
+#tokenized_input=tokenizer(text_input, padding=True, truncation=True, return_tensors="pt")
+#print(tokenized_input)
+
+#print("Running model to get tokenized output...\n")
+#output=model(**tokenized_input)
+
+#print(f"output is {output}")
+#print(f"SoftMax is {torch.nn.Softmax(output[0],dim=-1)}")
